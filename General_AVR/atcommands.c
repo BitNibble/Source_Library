e39c51e7a71febaf829347e30ae2940a21baf591
Sergio Santos <sergio.salazar.santos@gmail.com>
2023-12-29 14:02:07 +0000

    hc05cmd 2912023

--- General_AVR/atcommands.c
+++ General_AVR/atcommands.c
@@ -29,6 +29,7 @@ char ATstr[72];
 char* BT05ATcmd(uint16_t num){
 	switch(num)
 	{
+		/*** Query ***/
 		case 1:
 			strcpy(ATstr, "AT\r\n");
 		break;
@@ -36,177 +37,183 @@ char* BT05ATcmd(uint16_t num){
 			strcpy(ATstr, "AT+VERSION\r\n");
 		break;
 		case 3:
-			strcpy(ATstr, "AT+NAME\r\n"); // Param ? module Bluetooth name Default: MLT-BT05 longest ? 18 byte
+		//	strcpy(ATstr, "AT+BAND\r\n"); // Param ? MAC address
 		break;
 		case 4:
-			strcpy(ATstr, "AT+PIN\r\n"); // Param ? 6 byte bit pairing password default “? 123456”
-		break;
-		case 5: // Very usefull
-			strcpy(ATstr, "AT+GETSTAT\r\n"); // Param ? 0-1 , 0——not connected, 1——connected
-		break;
-		case 10:
-			strcpy(ATstr, "AT+ROLE\r\n"); // Param ?? 0-4 ?0 ——slave mode?, 1 ——master mode?, 2 ——sensor mode, 3 ——iBeacon mode, 4 ——WeChat mode, default ? 0
-		break;
-		case 101:
-			strcpy(ATstr, "AT+ROLE0\r\n"); // 0 ——slave mode
-		break;
-		case 102:
-			strcpy(ATstr, "AT+ROLE1\r\n"); // 1 ——master mode
-		break;
-		case 103:
-			strcpy(ATstr, "AT+ROLE2\r\n"); // 2 ——sensor mode
-		break;
-		case 104:
-			strcpy(ATstr, "AT+ROLE3\r\n"); // 3 ——iBeacon mode
-		break;
-		case 105:
-			strcpy(ATstr, "AT+ROLE4\r\n"); // 4 ——WeChat mode
-		case 11:
-			strcpy(ATstr, "AT+LADDR\r\n");
-		break;
-		case 12:
-			strcpy(ATstr, "AT+ADVI\r\n"); // 0 ——100ms,1 ——500ms,2 ——7500ms,3 ——1000ms,4 ——2000ms,5 ——4000ms,6 ——8000ms
-		break;
-		case 121:
-			strcpy(ATstr, "AT+ADVI0\r\n"); // 0 ——100ms
-		break;
-		case 122:
-			strcpy(ATstr, "AT+ADVI1\r\n"); // 1 ——500ms
-		break;
-		case 123:
-			strcpy(ATstr, "AT+ADVI2\r\n"); // 2 ——7500ms
-		break;
-		case 124:
-			strcpy(ATstr, "AT+ADVI3\r\n"); // 3 ——1000ms
-		break;
-		case 125:
-			strcpy(ATstr, "AT+ADVI4\r\n"); // 4 ——2000ms
-		break;
-		case 126:
-			strcpy(ATstr, "AT+ADVI5\r\n"); // 5 ——4000ms
-		break;
-		case 127:
-			strcpy(ATstr, "AT+ADVI6\r\n"); // 6 ——8000ms
-		break;
-		case 13:
-			strcpy(ATstr, "AT+TYPE\r\n"); // 0——no password,1——password pairing,2——password pairing and binding, default ? 0
-		break;
-		case 131:
-			strcpy(ATstr, "AT+TYPE0\r\n"); // 0——no password
-		break;
-		case 132:
-			strcpy(ATstr, "AT+TYPE1\r\n"); // 1——password pairing
-		break;
-		case 133:
-			strcpy(ATstr, "AT+TYPE2\r\n"); // 2——password pairing and binding
-		break;
-		case 14:
-			strcpy(ATstr, "AT+BAUD\r\n"); // 0 ——115200,1 ——57600,2 ——38400,3 ——19200,4 —— 9600, default ? 0
-		break;
-		case 141: // zs-040: works. 1200 hidden
-			//strcpy(ATstr, "AT+BAUD1\r\n");
-		break;
-		case 142: // zs-040: works. 2400 hidden
-			//strcpy(ATstr, "AT+BAUD2\r\n");
-		break;
-		case 143: // zs-040: works. 4800 hidden
-			//strcpy(ATstr, "AT+BAUD3\r\n");
-		break;
-		case 144: // zs-040: works. 9600 hidden
-			//strcpy(ATstr, "AT+BAUD4\r\n");
-		break;
-		case 145: // zs-040: works. 19200 hidden
-			//strcpy(ATstr, "AT+BAUD5\r\n");
-		break;
-		case 146: // zs-040: works. 38400 hidden
-			strcpy(ATstr, "AT+BAUD6\r\n");
-		break;
-		case 147: // zs-040: works. 57600 hidden
-			//strcpy(ATstr, "AT+BAUD7\r\n");
-		break;
-		case 148: // zs-040: works. 115200 hidden
-			//strcpy(ATstr, "AT+BAUD8\r\n");
-		break;
-		case 15:
 			strcpy(ATstr, "AT+UUID\r\n"); // Param: 0x0001~0xFFFE, default ? 0xFFE0
 		break;
-		case 16:
+		case 5:
 			strcpy(ATstr, "AT+CHAR\r\n"); // Param:0x0001~0xFFFE, default ? 0xFFE1
 		break;
-		case 17:
-			strcpy(ATstr, "AT+IBSUUID\r\n"); // Param ?FDA50693A4E24FB1AFCFC6EB07647825
+		case 6:
+			strcpy(ATstr, "AT+LADDR\r\n");
 		break;
-		case 18:
-			strcpy(ATstr, "AT+MAJOR\r\n"); // Param : 0x0001~0xFFFE, default ? 0x0007
+		case 7:
+			strcpy(ATstr, "AT+ROLE\r\n"); // Param ?? 0-4 ?0 ——slave mode?, 1 ——master mode?, 2 ——sensor mode, 3 ——iBeacon mode, 4 ——WeChat mode, default ? 0
 		break;
-		case 19:
-			strcpy(ATstr, "AT+MINOR\r\n"); // Param: 0x0001~0xFFFE, default ? 0x000A
+		case 8:
+			strcpy(ATstr, "AT+TYPE\r\n"); // 0——no password,1——password pairing,2——password pairing and binding, default ? 0
 		break;
-		case 20:
-			strcpy(ATstr, "AT+SLEEP\r\n"); // when PM=1.
+		case 9:
+			strcpy(ATstr, "AT+BAUD\r\n"); // 0 ——115200,1 ——57600,2 ——38400,3 ——19200,4 —— 9600, default ? 0
 		break;
-		case 21:
+		case 10:
+			strcpy(ATstr, "AT+ADVI\r\n"); // 0 ——100ms,1 ——500ms,2 ——7500ms,3 ——1000ms,4 ——2000ms,5 ——4000ms,6 ——8000ms
+		break;
+		case 11:
+			strcpy(ATstr, "AT+NAME\r\n"); // Param ? module Bluetooth name Default: MLT-BT05 longest ? 18 byte
+		break;
+		case 12:
+			strcpy(ATstr, "AT+PIN\r\n"); // Param ? 6 byte bit pairing password default “? 123456”
+		break;
+		case 13:
 			strcpy(ATstr, "AT+INQ\r\n"); // Param ? 1-0, 1: start scanning, 0: stop scanning
 		break;
-		case 211:
-			strcpy(ATstr, "AT+INQ0\r\n"); // 0: stop scanning
-		break;
-		case 212:
-			strcpy(ATstr, "AT+INQ1\r\n"); // 1: start scanning
-		break;
-		case 22:
-			//strcpy(ATstr, "AT+CONN<Param>\r\n"); // Param ? 0-7
-		break;
-		case 23:
-			//strcpy(ATstr, "AT+RSLV< Param >\r\n"); // Param ? ( 0-7 )
-		break;
-		case 24:
-			strcpy(ATstr, "AT+BAND\r\n"); // Param ? MAC address
-		break;
-		case 25:
+		case 14:
 			strcpy(ATstr, "AT+CLRBAND\r\n"); // no
 		break;
-		case 26:
-			//strcpy(ATstr, "AT+GETDCN<Param>\r\n"); // Param ?? 1-8 ?
+		case 15:
+			strcpy(ATstr, "AT+SLEEP\r\n"); // when PM=1.
 		break;
-		case 27:
-			strcpy(ATstr, "AT+DISC\r\n"); // Disconnect no
-		break;
-		case 28:
-			//strcpy(ATstr, "AT+AT_WXSWH<Param>\r\n"); // Param ?? 0-1), 0 ? H5 communication, 1 ? factory server, Default: 0
-		break;
-		case 29:
+		case 16:
 			strcpy(ATstr, "AT+RESET\r\n");
 		break;
-		case 30: // hiden
-			//strcpy(ATstr, "AT+DEFAULT\r\n"); // 4dbm, 9600bps (115200bps), 200ms, PM=0, Onboard LED 2:1, External indicator led1: 2
+		/*** while connected ***/
+		case 17: // Very usefull
+			strcpy(ATstr, "AT+GETSTAT\r\n"); // Param ? 0-1 , 0——not connected, 1——connected
 		break;
-		case 31:
-			strcpy(ATstr, "AT+HELP\r\n"); // Lists commands
+		case 18:
+			strcpy(ATstr, "AT+DISC\r\n"); // Disconnect no
 		break;
-		case 801:
-			strcpy(ATstr, "AT+PIN123456\r\n");
+		/****** SETUP ******/
+		case 71:
+			strcpy(ATstr, "AT+ROLE0\r\n"); // 0 ——slave mode
 		break;
-		case 802:
-			strcpy(ATstr, "AT+PIN654321\r\n");
+		case 72:
+			strcpy(ATstr, "AT+ROLE1\r\n"); // 1 ——master mode
 		break;
-		case 803:
-			strcpy(ATstr, "AT+PIN916919\r\n");
+		case 73:
+			strcpy(ATstr, "AT+ROLE2\r\n"); // 2 ——sensor mode
 		break;
-		case 901:
+		case 74:
+			strcpy(ATstr, "AT+ROLE3\r\n"); // 3 ——iBeacon mode
+		break;
+		case 75:
+			strcpy(ATstr, "AT+ROLE4\r\n"); // 4 ——WeChat mode
+		break;
+		case 81:
+			strcpy(ATstr, "AT+TYPE0\r\n"); // 0——no password
+		break;
+		case 82:
+			strcpy(ATstr, "AT+TYPE1\r\n"); // 1——password pairing
+		break;
+		case 83:
+			strcpy(ATstr, "AT+TYPE2\r\n"); // 2——password pairing and binding
+		break;
+		case 91: // zs-040: works. 1200 hidden
+		//	strcpy(ATstr, "AT+BAUD1\r\n");
+		break;
+		case 92: // zs-040: works. 2400 hidden
+		//	strcpy(ATstr, "AT+BAUD2\r\n");
+		break;
+		case 93: // zs-040: works. 4800 hidden
+		//	strcpy(ATstr, "AT+BAUD3\r\n");
+		break;
+		case 94: // zs-040: works. 9600 hidden
+		//	strcpy(ATstr, "AT+BAUD4\r\n");
+		break;
+		case 95: // zs-040: works. 19200 hidden
+		//	strcpy(ATstr, "AT+BAUD5\r\n");
+		break;
+		case 96: // zs-040: works. 38400 hidden
+			strcpy(ATstr, "AT+BAUD6\r\n");
+		break;
+		case 97: // zs-040: works. 57600 hidden
+		//	strcpy(ATstr, "AT+BAUD7\r\n");
+		break;
+		case 98: // zs-040: works. 115200 hidden
+		//	strcpy(ATstr, "AT+BAUD8\r\n");
+		break;
+		case 101:
+			strcpy(ATstr, "AT+ADVI0\r\n"); // 0 ——100ms
+		break;
+		case 102:
+			strcpy(ATstr, "AT+ADVI1\r\n"); // 1 ——500ms
+		break;
+		case 103:
+			strcpy(ATstr, "AT+ADVI2\r\n"); // 2 ——7500ms
+		break;
+		case 104:
+			strcpy(ATstr, "AT+ADVI3\r\n"); // 3 ——1000ms
+		break;
+		case 105:
+			strcpy(ATstr, "AT+ADVI4\r\n"); // 4 ——2000ms
+		break;
+		case 106:
+			strcpy(ATstr, "AT+ADVI5\r\n"); // 5 ——4000ms
+		break;
+		case 107:
+			strcpy(ATstr, "AT+ADVI6\r\n"); // 6 ——8000ms
+		break;
+		case 111:
 			strcpy(ATstr, "AT+NAMEBT05\r\n"); // Param ? module Bluetooth name Default: BT05 longest ? 18 byte
 		break;
-		case 902:
+		case 112:
 			strcpy(ATstr, "AT+NAMEHome1\r\n"); // Param ? module Bluetooth name Default: BT05 longest ? 18 byte
 		break;
-		case 903:
+		case 113:
 			strcpy(ATstr, "AT+NAMEHome2\r\n"); // Param ? module Bluetooth name Default: BT05 longest ? 18 byte
 		break;
-		case 904:
+		case 114:
 			strcpy(ATstr, "AT+NAMEHome3\r\n"); // Param ? module Bluetooth name Default: BT05 longest ? 18 byte
 		break;
-		case 905:
+		case 115:
 			strcpy(ATstr, "AT+NAMEHome4\r\n"); // Param ? module Bluetooth name Default: BT05 longest ? 18 byte
+		break;
+		case 121:
+			strcpy(ATstr, "AT+PIN123456\r\n");
+		break;
+		case 122:
+			strcpy(ATstr, "AT+PIN654321\r\n");
+		break;
+		case 123:
+			strcpy(ATstr, "AT+PIN916919\r\n");
+		break;
+		case 131:
+			strcpy(ATstr, "AT+INQ0\r\n"); // 0: stop scanning
+		break;
+		case 132:
+			strcpy(ATstr, "AT+INQ1\r\n"); // 1: start scanning
+		break;
+		case 19:
+			strcpy(ATstr, "AT+IBSUUID\r\n"); // Param ?FDA50693A4E24FB1AFCFC6EB07647825
+		break;
+		case 20:
+			strcpy(ATstr, "AT+MAJOR\r\n"); // Param : 0x0001~0xFFFE, default ? 0x0007
+		break;
+		case 21:
+			strcpy(ATstr, "AT+MINOR\r\n"); // Param: 0x0001~0xFFFE, default ? 0x000A
+		break;
+		case 22:
+		//	strcpy(ATstr, "AT+CONN<Param>\r\n"); // Param ? 0-7
+		break;
+		case 23:
+		//	strcpy(ATstr, "AT+RSLV< Param >\r\n"); // Param ? ( 0-7 )
+		break;
+		case 24:
+		//	strcpy(ATstr, "AT+GETDCN<Param>\r\n"); // Param ?? 1-8 ?
+		break;
+		case 25:
+		//	strcpy(ATstr, "AT+AT_WXSWH<Param>\r\n"); // Param ?? 0-1), 0 ? H5 communication, 1 ? factory server, Default: 0
+		break;
+		/*** Test ***/
+		
+		/*** Preamble ***/
+		case 26: // hidden
+		//	strcpy(ATstr, "AT+DEFAULT\r\n"); // 4dbm, 9600bps (115200bps), 200ms, PM=0, On board LED 2:1, External indicator led1: 2
+		break;
+		case 27:
+			strcpy(ATstr, "AT+HELP\r\n"); // Lists commands
 		break;
 		/******/
 		default:
