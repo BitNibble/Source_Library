fc61e28ec945b669089de5b46fd67d937ddbdb0d
Sergio Santos <sergio.salazar.santos@gmail.com>
2024-01-11 14:22:03 +0000

    arm rcc upgrade 11012024

---
+++ stm32f446lib/Core/Src/main.c
@@ -0,0 +1,589 @@
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+#include "main.h"
+/*** Library Switches ***/
+#ifndef ARM_MATH_CM4
+	#define ARM_MATH_CM4
+#endif
+
+#include "stm32446mapping.h"
+#include "armfunction.h"
+#include "explode.h"
+#include "74hc595.h"
+#include "armlcd.h"
+#include "circbuffer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+#ifndef ass
+	#define ass (1)
+#endif
+
+static EXPLODE PINA;
+static EXPLODE PINB;
+static EXPLODE PINC;
+static HC595 hc;
+static ARMLCD0 lcd;
+static circbuff circ1;
+static circbuff circ2;
+static char buffer1[32]; // for circular buffer
+static char buffer2[32]; // for circular buffer
+
+volatile unsigned int time_out;
+static uint32_t index_var;
+static uint32_t test_var;
+static uint8_t choice;
+static uint8_t hour = 0;
+static uint8_t minute = 0;
+static uint8_t second = 0;
+static uint8_t ano = 0;
+static uint8_t mes = 1;
+static uint8_t dia = 1;
+static uint32_t value = 0;
+
+uint16_t temperature[128];
+uint32_t valuet;
+double tempt;
+
+static char transmit;
+//static char toggle;
+static char receive;
+static char received[32]; // from circular buffer
+static char message[64];
+
+static int8_t count1;
+static uint8_t count2;
+static uint8_t dir;
+uint16_t charcount;
+
+uint8_t button_press;
+
+void portinic(void);
+void calendario(void);
+void USART1_IRQHandler(void);
+
+/*************************************/
+int main(void)
+{
+STM32446enable();
+FUNCenable();
+portinic();
+
+unsigned int zone;
+
+button_press = 0;
+charcount=0;
+time_out = 0;
+test_var = 0;
+
+//func = FUNCenable();
+PINA = EXPLODEenable();
+PINB = EXPLODEenable();
+PINC = EXPLODEenable();
+circ1 = CIRCBUFFenable(8, buffer1);
+circ2 = CIRCBUFFenable(8, buffer2);
+
+// FPU full access
+func()->setreg(&stm()->scb.reg->CPACR, 2, 20, 3);
+func()->setreg(&stm()->scb.reg->CPACR, 2, 22, 3);
+
+choice = 3;
+count1 = 0;
+count2 = 0;
+dir = 0;
+
+hc = HC595enable(&stm()->gpioc.reg->MODER, &stm()->gpioc.reg->ODR, 2, 1, 0);
+lcd = ARMLCD0enable(stm()->gpiob.reg);
+
+
+circ1.putstr(&circ1.par, "Welcome\r\n");
+//stm()->rtc.run->BckWrite(2,33);
+//stm.func.setreg(&test_var,2,30,3);
+
+/*************************************/
+for ( zone = 0 ; ass ; zone++)
+{// COMMON
+
+if(zone == 1){ // Preamble
+	/*** PREAMBLE PREAMBLE COMMON IO ***/
+	PINA.update(&PINA.par, stm()->gpioa.reg->IDR);
+	PINB.update(&PINB.par, stm()->gpiob.reg->IDR);
+	PINC.update(&PINC.par, stm()->gpioc.reg->IDR);
+	lcd.reboot();
+}
+/******************************************************************************/
+/******************************************************************************/
+if(zone == 2){ // workspace 1 RTC CALENDAR
+
+	//if(PINC.par.LH & (1 << 13)){lcd.gotoxy(0,11); lcd.string(func.i32toa(stm.query.PLL_parameter.Source));}
+	if(PINC.par.LH & (1 << 13)){
+		//lcd0()->gotoxy(0,11); lcd0()->string_size(stm()->func->i16toa(button_press++),9);
+		//lcd.gotoxy(0,11); lcd.string_size(stm.func.i16toa(button_press++),4);
+		circ1.putstr(&circ1.par, "Welcome\r\n");
+		//transmit = 'Z';
+	}
+	//lcd.gotoxy(0,11); lcd.string_size(stm.func.i32toa(stm.tim8.get_cnt()),5);
+	//lcd.gotoxy(3,15); lcd.string_size(stm()->func->i32toa(stm()->func->value()),5);
+	//lcd.gotoxy(0,15); lcd.string(stm.func.i32toa(test_var));
+	//lcd.gotoxy(0,15); lcd.string(stm.func.i32toa(stm.query.SystemClock()));
+	lcd.gotoxy(0,15); lcd.string(func()->i32toa(stm()->rtc->BckRead(2)));
+	//lcd.gotoxy(0,15); lcd.string(stm.func.i32toa(stm.func.getsetbit(&regist, 2, 24)));
+	//lcd.gotoxy(0,11); lcd.string(stm.func.ftoa((double)regist,message,1));
+	//if(PINC.par.LL & (1 << 13)){stm.func.setbit(&stm.gpioa.reg->ODR, 1, 5, 2);}
+
+	calendario();
+
+} // if
+/******************************************************************************/
+/******************************************************************************/
+if(zone == 3){ // workspace 2 ADC1 TEMPERATURE
+	for(tempt=0, index_var = 0; index_var < 32; index_var += 2 ){
+		tempt += temperature[index_var];
+	}
+	valuet = tempt / 16;
+	valuet = (valuet/3.1 - 943/3.1) + 25; // (valuet/3.1 - 943/3.1) + 25;
+
+	lcd.gotoxy(1,0);
+	lcd.string_size( func()->print_v1(message, 10, "%d %cC", (unsigned int)valuet, (char) 0xDF ), 6);
+	//lcd.string_size( stm.func.print_v1(message, 10, "%d %cC", (unsigned int)tempt, (char) 0xDF ), 10);
+	//lcd.gotoxy(1,6);
+	lcd.string_size( func()->print_v1(message, 10, "%d", (unsigned int)temperature[3], (char) 0xDF ), 4);
+} // if
+/******************************************************************************/
+/******************************************************************************/
+if(zone == 4){ // workspace 3 USART1 TX RX
+
+	lcd.gotoxy(1,11);
+	lcd.string_size(received, 9);
+	if( stm()->usart1->sr.tc() ){ // TC: Transmission complete
+		transmit = circ1.get(&circ1.par);
+		if(transmit){ stm()->usart1->dr(transmit); for(time_out = 20; (!stm()->usart1->sr.tc()) && time_out; time_out--);}
+		}
+		zone = 0;
+	} // if
+/******************************************************************************/
+/******************************************************************************/
+} // for
+} // main
+/******************************************************************************/
+/******************************************************************************/
+void portinic(void)
+{
+	//Enable clock for IO peripherals
+	stm()->rcc->apb1enr->pwren(yes);
+	stm()->rcc->apb2enr->usart1en(yes);
+	stm()->rcc->bdcr->rtcen(yes);
+	stm()->rcc->apb2enr->adc1en(yes);
+	stm()->rcc->ahb1enr->gpioaen(yes);
+	stm()->rcc->ahb1enr->gpioben(yes);
+	stm()->rcc->ahb1enr->gpiocen(yes);
+	stm()->rcc->apb2enr->tim9en(yes);
+	/***************************/
+	stm()->gpioa.clock(on);
+	stm()->gpiob.clock(on);
+	stm()->gpioc.clock(on);
+  	// PA5 or PB13 is green user led
+	stm()->gpioa.moder(1,5);
+	stm()->gpioa.pupdr(0,5);
+	//stm.gpiob.moder(1,13);
+	// PC13 is user button
+	stm()->gpioc.moder(0,13);
+	stm()->gpioc.pupdr(1,13);
+	/********** USART1 *********/
+	stm()->usart1->clock(on);
+	stm()->nvic.set_enable(37);
+	stm()->usart1->cr1.ue(on);
+	stm()->gpioa.moder(2,9);
+	stm()->gpioa.moder(2,10);
+	stm()->gpioa.afr(7,9);
+	stm()->gpioa.afr(7,10);
+	stm()->usart1->parameter(8, 16, 1, 38400);
+	stm()->usart1->cr3.dmat(off);
+	stm()->usart1->cr1.te(on);
+	stm()->usart1->cr3.dmar(off);
+	stm()->usart1->cr1.re(on);
+	stm()->usart1->cr1.rxneie(on);
+	/******* TIMER9 SETUP ******/
+	stm()->tim9.clock(on);
+	// NVIC
+	stm()->tim9.nvic(on);
+	// Frequency
+	stm()->tim9.arr(45535);
+	// Compare
+	stm()->tim9.ccr1(7530);
+	// prescaler
+	stm()->tim9.psc(20);
+	// interrupt
+	stm()->tim9.dier.cc1ie(on);
+	// Enable (Start/Stop)
+	stm()->tim9.cr1.apre(on);
+	stm()->tim9.cr1.cen(on);
+	/********** ADC1 ***********/
+	stm()->gpioa.pupdr(1,0);
+	stm()->gpioa.moder(3,0);
+	// ADC Clock
+	//STM32446Adc1IClock(1); // DACEN: DAC interface clock enable
+	STM32446Adc1Clock(1); // ADC1EN: ADC1 clock enable
+
+	STM32446ADC1_cr2_adon(0); // ADON: A/D Converter ON / OFF
+
+	STM32446ADC_ccr_adcpre(3); // ADCPRE: ADC prescaler, 11: PCLK2 divided by 8
+
+	STM32446ADC1_smpr1_smp18(7); // SMPx[2:0]: Channel x sampling time selection
+	STM32446ADC1_smpr2_smp0(7); // SMPx[2:0]: Channel x sampling time selection
+
+	STM32446ADC1_sqr1_l(1); // 0 -> read one channel, 1 -> read two channels
+	STM32446ADC1_sqr3_sq1(18); // SQ1[4:0]: 1st conversion in regular sequence
+	STM32446ADC1_sqr3_sq2(0); // SQ2[4:0]: 2st conversion in regular sequence
+
+	stm()->adc1->single->temp();
+	//stm.adc1.single.vbat();
+	STM32446ADC1_cr2_eocs(1);
+	stm()->adc1->cr2->dma(1);
+	stm()->adc1->cr2->dds(1);
+	// turn on select source and start reading
+	STM32446Adc1Start();
+	stm()->adc1->cr2->cont(1);
+	stm()->adc1->cr1->scan(1);
+
+	stm()->dma2.clock(on);
+	stm()->dma2.func.circ_cfg(&ADC1->DR, (volatile long unsigned int*)temperature, 0, 0, 32, 1, 1, 0, 0);
+
+	/*********** RTC ***********/
+	// Three ways to kill a rabbit
+	//stm()->rtc.run()->clock(on);
+	stm()->rtc->clock(on);
+	//rtc()->clock(on);
+	//stm()->rtc.run()->inic(1); // 2 - LSI, 1 - LSE
+	rtc()->inic(1); // 2 - LSI, 1 - LSE
+	//rtc()->inic(1); // 2 - LSI, 1 - LSE
+	/***************************/
+}
+/******************************************************************************/
+void calendario(void)
+{
+	char vecT[8]; // for calendar
+	char vecD[8]; // for calendar
+	const uint16_t min1 = 0;
+	const uint16_t max1 = 50;
+	const uint16_t min2 = 50;
+	const uint16_t max2 = 120;
+	const uint16_t min3 = 120;
+	const uint16_t max3 = 500;
+
+	const uint16_t minp1 = 0;
+	const uint16_t maxp1 = 50;
+	const uint16_t minp2 = 50;
+	const uint16_t maxp2 = 120;
+	const uint16_t minp3 = 120;
+	//const uint16_t maxp3 = 100;
+
+	/******MENU*****/
+	switch(choice){
+		case 1: // show time
+			lcd.gotoxy(0,0);
+			lcd.string_size("Relogio",7);
+			rtc()->tr2vec(vecT);
+			lcd.gotoxy(3,0);
+			lcd.string_size(func()->print_v2("hora: %d%d:%d%d:%d%d", vecT[0],vecT[1],vecT[2],vecT[3],vecT[4],vecT[5]),14);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > min1 && value < max1 ){
+				lcd.clear();
+				rtc()->dr2vec(vecD);
+				circ1.putstr(&circ1.par, func()->print_v2("data: %d%d:%d%d:20%d%d\r\n", vecD[5],vecD[6],vecD[3],vecD[4],vecD[0],vecD[1]) );
+				choice = 2;
+			}
+			if( value > min2 && value < max2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "acertar hora\r\n");
+				choice = 4;
+			}
+			if( value > min3 && value < max3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Calendario\r\n");
+				choice = 3;
+			}
+			break;
+
+		case 2: // show date
+			lcd.gotoxy(0,0);
+			lcd.string_size("Data",4);
+			rtc()->dr2vec(vecD);
+			lcd.gotoxy(3,0);
+			lcd.string_size(func()->print_v2("data: %d%d:%d%d:20%d%d", vecD[5],vecD[6],vecD[3],vecD[4],vecD[0],vecD[1]),16);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > min1 && value < max1 ){
+				lcd.clear();
+				rtc()->tr2vec(vecT);
+				circ1.putstr(&circ1.par, func()->print_v2("hora: %d%d:%d%d:%d%d\r\n", vecT[0],vecT[1],vecT[2],vecT[3],vecT[4],vecT[5]) );
+				choice = 1;
+			}
+			if( value > min2 && value < max2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "acertar ano\r\n");
+				choice = 7;
+			}
+			if( value > min3 && value < max3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Calendario\r\n");
+				choice = 3;
+			}
+			break;
+
+		case 3: // message
+			lcd.gotoxy(0,0);
+			lcd.string_size("Cal",3);
+
+			rtc()->dr2vec(vecD);
+			lcd.gotoxy(2,0);
+			lcd.string_size(func()->print_v2("data: %d%d:%d%d:20%d%d", vecD[5],vecD[6],vecD[3],vecD[4],vecD[0],vecD[1]), 16);
+
+			rtc()->tr2vec(vecT);
+			lcd.gotoxy(3,0);
+			lcd.string_size(func()->print_v2("hora: %d%d:%d%d:%d%d", vecT[0],vecT[1],vecT[2],vecT[3],vecT[4],vecT[5]), 14);
+
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > min3 && value < max3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Relogio\r\n");
+				choice = 1;
+			}
+			break;
+
+		// Relogio
+		case 4: // Set Hour
+			lcd.gotoxy(0,0);
+			lcd.string_size("Hora:",5);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > minp1 && value < maxp1 ){
+				hour ++;
+				if(hour > 23)
+					hour = 0;
+				lcd.gotoxy(2,0);
+				lcd.string_size(func()->print_v2("hora: %d", hour),8);
+			}
+			if( value > minp2 && value < maxp2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "acertar minutos\r\n");
+				choice = 5;
+			}
+			if( value > minp3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Relogio\r\n");
+				choice = 1;
+				rtc()->Hour(hour);
+			}
+			break;
+
+		case 5: // Set Minute
+			lcd.gotoxy(0,0);
+			lcd.string_size("Minutos:",8);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > minp1 && value < maxp1 ){
+				minute ++;
+				if(minute > 59)
+					minute = 0;
+				lcd.gotoxy(2,0);
+				lcd.string_size(func()->print_v2("minuto: %d", minute),10);
+			}
+			if( value > minp2 && value < maxp2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "acertar segundos\r\n");
+				choice = 6;
+			}
+			if( value > minp3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Relogio\r\n");
+				choice = 1;
+				rtc()->Minute(minute);
+			}
+			break;
+
+		case 6: // Set Second
+			lcd.gotoxy(0,0);
+			lcd.string_size("Segundos:",9);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > minp1 && value < maxp1 ){
+				second ++;
+				if(second > 59)
+					second = 0;
+				lcd.gotoxy(2,0);
+				lcd.string_size(func()->print_v2("segundo: %d", second),11);
+			}
+			if( value > minp2 && value < maxp2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Relogio\r\n");
+				choice = 1;
+			}
+			if( value > minp3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Relogio\r\n");
+				choice = 1;
+				rtc()->Second(second);
+			}
+			break;
+
+			// Calendario
+		case 7: // Set Year
+			lcd.gotoxy(0,0);
+			lcd.string_size("Ano:",4);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > minp1 && value < maxp1 ){
+				ano ++;
+				if(ano > 99)
+					ano = 0;
+				lcd.gotoxy(2,0);
+				lcd.string_size(func()->print_v2("Ano: %d", ano),7);
+			}
+			if( value > minp2 && value < maxp2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "acertar mes\r\n");
+				choice = 8;
+			}
+			if( value > minp3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Data\r\n");
+				choice = 2;
+				rtc()->Year(ano);
+			}
+			break;
+
+		case 8: // Set Month
+			lcd.gotoxy(0,0);
+			lcd.string_size("Mes:",4);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > minp1 && value < maxp1 ){
+				mes ++;
+				if(mes > 12)
+					mes = 1;
+				lcd.gotoxy(2,0);
+				lcd.string_size(func()->print_v2("mes: %d", mes),7);
+			}
+			if( value > minp2 && value < maxp2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "acertar dia\r\n");
+				choice = 9;
+			}
+			if( value > minp3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Data\r\n");
+				choice = 2;
+				rtc()->Month(mes);
+			}
+			break;
+
+		case 9: // Set Day
+			lcd.gotoxy(0,0);
+			lcd.string_size("Dia:",4);
+			value = func()->triggerA(PINC.par.LL,13,rtc()->get_ss());
+			if( value > minp1 && value < maxp1 ){
+				dia ++;
+				if(dia > 31)
+					dia = 1;
+				lcd.gotoxy(2,0);
+				lcd.string_size(func()->print_v2("dia: %d", dia),7);
+			}
+			if( value > minp2 && value < maxp2 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Data\r\n");
+				choice = 2;
+			}
+			if( value > minp3 ){
+				lcd.clear();
+				circ1.putstr(&circ1.par, "Data\r\n");
+				choice = 2;
+				rtc()->Day(dia);
+			}
+			break;
+
+		default:
+			break;
+	}
+
+}
+/******************************************************************************/
+/*** TIM9 Interrupt Definition ***/
+void TIM1_BRK_TIM9_IRQHandler(void)
+{
+	if(stm()->tim9.sr.uif()){ // status register (view interrupt flags)
+		stm()->tim9.sr.clear_uif();
+
+		if(dir){
+			stm()->gpioa.reset(1 << 5);
+
+			hc.byte(&hc.par, (uint8_t)~(1 << count1--));
+			if(count1 < 0)
+				dir = 0;
+		}else{
+			stm()->gpioa.set(1 << 5);
+
+			hc.byte(&hc.par, (uint8_t)~(1 << count1++));
+			if(count1 > 7)
+				dir = 1;
+		}
+		count2++;
+	}
+	if(stm()->tim9.sr.cc1if()){
+		stm()->tim9.sr.clear_cc1if();
+
+
+	}
+	//stm.tim9.reg->SR &=  (uint32_t) ~1;
+	//stm.tim9.reg->SR &=  (uint32_t) ~2;
+}
+/******************************************************************************/
+void USART1_IRQHandler(void)
+{
+	charcount++;
+	if( stm()->usart1->sr.rxne() ){ // RXNE: Read data register not empty
+		receive = stm()->usart1->get_dr();
+		if(receive){
+			circ2.put(&circ2.par, receive);
+
+			if(receive == 13 ){
+				circ2.getstr(&circ2.par, received);
+				received[strlen(received)-1] = '\0'; //remove enter key [13]
+			}
+		}
+	}
+	if( stm()->usart1->sr.ore() ){ receive = usart1()->get_dr(); }
+
+}
+/******************************************************************************/
+void Error_Handler(void)
+{
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ stm32f446lib/Library/Src/stm32446mapping.c
@@ -0,0 +1,200 @@
+/******************************************************************************
+	STM32 446 MAPPING
+Author: Sergio Santos 
+	<sergio.salazar.santos@gmail.com>
+License: GNU General Public License
+Hardware: STM32-446
+Date: 19/06/2023
+Update: 05/01/2024
+Comment:
+	-manual um1724, m0390, pm0056, pm0214, and other sources.
+	-Virtual Image STM32-446, mapping.
+	-Still in progress 15062023, increment has go along.
+	-Dependent on CMSIS
+	-Stable.
+*******************************************************************************/
+/*** File Library ***/
+#include "stm32446mapping.h"
+#include <stdarg.h>
+#include <math.h>
+/*** File Constant & Macros ***/
+// CPACR is located at address 0xE000ED88
+#define STM32446_SCB_BASE ((0xE000E000UL) + 0x0D00UL))
+/*** File Variable ***/
+static STM32446 stm32446;
+
+/*** File Header ***/
+
+/******* STM32F446RE Procedure & Function Definition *******/
+STM32446 STM32446enable(void){
+	/************* CORE ************/
+	// SCB
+	stm32446.scb.reg = ((SCB_Type*) STM32446_SCB_BASE;
+	// NVIC
+	#if defined(_STM32446NVIC_H_)
+		stm32446.nvic = nvic_inic();
+	#endif
+	/************ MCU ************/
+	// ADC -> ADC1
+	#if defined(_STM32446ADC1_H_)
+		adc1_inic();
+		stm32446.adc1 = adc1();
+	#endif
+	// ADC -> ADC2
+	#if defined(_STM32446ADC2_H_)
+		stm32446.adc2 = adc2_inic();
+	#endif
+	// ADC -> ADC3
+	#if defined(_STM32446ADC3_H_)
+		stm32446.adc3 = adc3_inic();
+	#endif
+	// ADC -> ADC123
+	#if defined(_STM32446ADC123_H_)
+		adc1_inic();
+		stm32446.adc1 = adc1();
+		stm32446.adc2 = adc2_inic();
+		stm32446.adc3 = adc3_inic();
+	#endif
+	// CRC
+	#if defined(_STM32446CRC_H_)
+		stm32446.crc = crc_inic();
+	#endif
+	#if defined(_STM32446DMA_H_)
+	// DMA1
+		stm32446.dma1 = dma1_inic();
+	// DMA2
+		stm32446.dma2 = dma2_inic();
+	#endif
+	// FLASH
+	#if defined(_STM32446FLASH_H_)
+		stm32446.flash = flash_inic();
+	#endif
+	// GPIO
+	#if defined(_STM32446GPIO_H_)
+		stm32446.gpioa = gpioa_inic();
+		stm32446.gpiob = gpiob_inic();
+		stm32446.gpioc = gpioc_inic();
+		stm32446.gpiod = gpiod_inic();
+		stm32446.gpioe = gpioe_inic();
+		stm32446.gpioh = gpioh_inic();
+	#endif
+	// SYSCFG
+	#if defined(_STM32446SYSCFG_H_)
+		stm32446.syscfg.reg = SYSCFG;
+	#endif
+	// PWR
+	#if defined(_STM32446PWR_H_)
+		stm32446.pwr = pwr_inic();
+	#endif
+	// RCC
+	#if defined(_STM32446RCC_H_)
+		rcc_inic();
+		stm32446.rcc = rcc();
+	#endif
+	// RTC
+	#if defined(_STM32446RTC_H_)
+		RTCenable();
+		stm32446.rtc = rtc();
+	#endif
+	// SRAM
+	#if defined(_STM32446SRAM_H_)
+		stm32446.sram = sram_inic();
+	#endif
+	// TIM
+	#if defined(_STM32446TIM1AND8_H_)
+		stm32446.tim1 = tim1_inic();
+		stm32446.tim8 = tim8_inic();
+	#endif
+	#if defined(_STM32446TIM2TO5_H_)
+		stm32446.tim2 = tim2_inic();
+		stm32446.tim3 = tim3_inic();
+		stm32446.tim4 = tim4_inic();
+		stm32446.tim5 = tim5_inic();
+	#endif
+	#if defined(_STM32446TIM6AND7_H_)
+		stm32446.tim6 = tim6_inic();
+		stm32446.tim7 = tim7_inic();
+	#endif
+	#if defined(_STM32446TIM9TO14_H_)
+		stm32446.tim9 = tim9_inic();
+		stm32446.tim10 = tim10_inic();
+		stm32446.tim11 = tim11_inic();
+		stm32446.tim12 = tim12_inic();
+		stm32446.tim13 = tim13_inic();
+		stm32446.tim14 = tim14_inic();
+	#endif
+	// USART
+	#if defined(_STM32446USART_H_)
+		USART1enable();
+		stm32446.usart1 = (STM32446_USART1*) usart1();
+		USART2enable();
+		stm32446.usart2 = (STM32446_USART2*) usart2();
+		USART3enable();
+		stm32446.usart3 = (STM32446_USART3*) usart3();
+		UART4enable();
+		stm32446.uart4 = (STM32446_UART4*) uart4();
+		UART5enable();
+		stm32446.uart5 = (STM32446_UART5*) uart5();
+		USART6enable();
+		stm32446.usart6 = (STM32446_USART6*) usart6();
+	#endif
+
+	query_inic();
+	systick_start();
+
+	return stm32446;
+}
+
+STM32446* stm(void){return (STM32446*) &stm32446;}
+
+/*** File Procedure & Function Definition ***/
+/***/
+void STM32446RegSetBits( unsigned int* reg, int n_bits, ... )
+{
+	uint8_t i;
+	if(n_bits > 0 && n_bits < 33){ // Filter input
+		va_list list;
+		va_start(list, n_bits);
+		for(i = 0; i < n_bits; i++){
+			*reg |= (unsigned int)(1 << va_arg(list, int));
+		}
+		va_end(list);
+	}
+}
+void STM32446RegResetBits( unsigned int* reg, int n_bits, ... )
+{
+	uint8_t i;
+	if(n_bits > 0 && n_bits < 33){ // Filter input
+		va_list list;
+		va_start(list, n_bits);
+		for(i = 0; i < n_bits; i++){
+			*reg &= (unsigned int)~((1 << va_arg(list, int)) << 16);
+		}
+		va_end(list);
+	}
+}
+void STM32446VecSetup( volatile uint32_t vec[], const unsigned int size_block, unsigned int data, unsigned int block_n )
+{
+	const unsigned int n_bits = sizeof(data) * 8;
+	const unsigned int mask = (unsigned int) (pow(2, size_block) - 1);
+	unsigned int index = (block_n * size_block) / n_bits;
+	data &= mask;
+	vec[index] &= ~( mask << ((block_n * size_block) - (index * n_bits)) );
+	vec[index] |= ( data << ((block_n * size_block) - (index * n_bits)) );
+}
+
+/***EOF***/
+
+/******
+1º Sequence
+2º Scope
+	- Library Scope
+	- File Scope
+	- Function Scope
+	- Precedence Scope
+3º Pointer and Variable
+4º Casting
+******/
+
+
+
