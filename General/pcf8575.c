405098235351d4671b315d4c1e031dd3c51b742b
Sergio Santos <sergio.salazar.santos@gmail.com>
2024-01-24 17:01:08 +0000

    pcf8575-1 22012024

--- General/pcf8575.c
+++ General/pcf8575.c
@@ -7,7 +7,7 @@ Hardware: PCF8575
 Date: 24012024
 ***************************************************************************************************/
 /****** Comment:
-	Stable
+	Allow to create up to four instances of pcf8575.
  **************************************************************************************************/
 /*** File Library ***/
 #include "pcf8575.h"
@@ -20,16 +20,23 @@ Date: 24012024
 #endif
 
 /*** File Header ***/
-void PCF8575_writehbits(uint16_t hbits, uint8_t bool);
-uint16_t PCF8575_readhbits(uint16_t hbits);
+void PCF8575_writehbits( pcf8575_parameter par, uint16_t hbits, uint8_t bool );
+uint16_t PCF8575_readhbits( pcf8575_parameter par, uint16_t hbits );
 
 #ifdef _TWI_MODULE_
 
 /*** Procedure & Function ***/
-PCF8575 pcf8575_enable(uint8_t prescaler)
+PCF8575 pcf8575_enable(uint8_t pcf8575_id, uint8_t twi_prescaler)
 {
 	PCF8575 setup_pcf8575;
-	twi_enable('A', prescaler); // Initialize the I2c module.
+	static uint8_t N_pcf8575 = 0;
+	twi_enable(85, twi_prescaler); // Initialize the I2c module.
+	
+	setup_pcf8575.par.pcf8575_id = pcf8575_id;
+	setup_pcf8575.par.N_pcf8575 = N_pcf8575;
+	
+	if(++N_pcf8575 > 3) N_pcf8575 = 3;
+	
 	// Vtable
 	setup_pcf8575.writehbits = PCF8575_writehbits;
 	setup_pcf8575.readhbits = PCF8575_readhbits;
@@ -37,22 +44,22 @@ PCF8575 pcf8575_enable(uint8_t prescaler)
 	return setup_pcf8575;
 }
 
-void PCF8575_writehbits(uint16_t hbits, uint8_t bool)
+void PCF8575_writehbits(pcf8575_parameter par, uint16_t hbits, uint8_t bool)
 {
-	static uint16_t mask = 0xFFFF;
-	if(bool) mask |= hbits; else mask &= ~hbits;
+	static uint16_t mask[N_Instance_PCF8575] = { 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF };
+	if(bool) mask[par.N_pcf8575] |= hbits; else mask[par.N_pcf8575] &= ~hbits;
 	twi()->start();
-	twi()->connect(PCF8575_ADDR,TWI_WRITE);
-	twi()->master_write(writelhbyte(mask).L);
-	twi()->master_write(writelhbyte(mask).H);
+	twi()->connect(par.pcf8575_id,TWI_WRITE);
+	twi()->master_write(writelhbyte(mask[par.N_pcf8575]).L);
+	twi()->master_write(writelhbyte(mask[par.N_pcf8575]).H);
 	twi()->stop();
 }
 
-uint16_t PCF8575_readhbits(uint16_t hbits)
+uint16_t PCF8575_readhbits(pcf8575_parameter par, uint16_t hbits)
 {
 	HighLowByte data;
 	twi()->start();
-	twi()->connect(PCF8575_ADDR, TWI_READ);
+	twi()->connect(par.pcf8575_id, TWI_READ);
 	data.L = twi()->master_read(TWI_ACK);
 	data.H = twi()->master_read(TWI_NACK);
 	twi()->stop();
--- General/pcf8575.h
+++ General/pcf8575.h
@@ -7,7 +7,7 @@ Hardware: PCF8575
 Date: 24012024
 ***************************************************************************************************/
 /****** Comment:
-	Stable
+	Allow to create up to four instances of pcf8575.
  **************************************************************************************************/
 #ifndef _PCF8575_H_
 	#define _PCF8575_H_
@@ -16,15 +16,21 @@ Date: 24012024
 #include <inttypes.h>
 
 /*** Global Constant & Macro ***/
-#define PCF8575_ADDR   32
+#define N_Instance_PCF8575 4
 
 typedef struct{
-	void (*writehbits)(uint16_t hbits, uint8_t bool);
-	uint16_t (*readhbits)(uint16_t hbits);
+	uint8_t pcf8575_id;
+	uint8_t N_pcf8575;
+}pcf8575_parameter;
+
+typedef struct{
+	pcf8575_parameter par;
+	void (*writehbits)( pcf8575_parameter par, uint16_t hbits, uint8_t bool );
+	uint16_t (*readhbits)( pcf8575_parameter par, uint16_t hbits );
 }PCF8575;
 
 /*** Global Header ***/
-PCF8575 pcf8575_enable(uint8_t prescaler);
+PCF8575 pcf8575_enable( uint8_t pcf8575_id, uint8_t twi_prescaler );
 
 #endif
 
